63f438a4ec54461bab18b9ebc42150d8a088bea4
Hnin Yamone Win <hninyamone278@gmail.com>
2026-05-09 14:42:47 +0630

    Fix cors

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://hninymojewelleryshop-qp8v.onrender.com',
+  credentials: true,
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
